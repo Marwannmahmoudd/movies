71598ae4742fcd9e0d948598ba417f79909d58fc
marwan7arbi <124735234+marwan7arbi@users.noreply.github.com>
2023-05-13 13:00:44 +0300

    upload

---
+++ src/app/compon/login-register/login-register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-login-register',
+  templateUrl: './login-register.component.html',
+  styleUrls: ['./login-register.component.css']
+})
+export class LoginRegisterComponent {
+ 
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+import { Observable ,BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
+import jwtDecode from 'jwt-decode';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  baseurl:string = "https://route-ecommerce.onrender.com/"
+  token:any
+ 
+  token4:any
+  token2:any
+  condit:BehaviorSubject<boolean> =new BehaviorSubject(false)
+  constructor(private _userservice:HttpClient, private _router:Router) { 
+  
+    
+    
+    if((localStorage.getItem('userdata') !== null || localStorage.getItem('email')) !== null){
+      this.token = localStorage.getItem('email')
+     
+     this.condit.next(true)
+     }
+     else{
+         this.token = null
+         this.condit.next(false)
+       if(_router.url === '/register'){
+       //  _router.navigate(['/register'])
+       }
+     }
+   }
+      
+       decode(){
+       let encode = JSON.stringify( localStorage.getItem('userdata'))
+        let decode = jwtDecode(encode)
+ this.token2.next(decode)
+ localStorage.setItem('token',this.token2)
+       }
+       
+       
+ 
+   loginout(){
+     localStorage.removeItem('userdata');
+     localStorage.removeItem('email');
+     this.token = null
+     this.condit.next(false)
+    this._router.navigate(['/login'])
+   }
+  
+  getdata(data:object):Observable<any>{
+    return this._userservice.post(this.baseurl + "api/v1/auth/signup" , data)
+    } 
+    sentdata(data:object):Observable<any>{
+     return this._userservice.post(this.baseurl + "api/v1/auth/signin" , data)
+     } 
+    
+}
